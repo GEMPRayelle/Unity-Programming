5c1c1e8ca2d6ddeb94f2c13bb772a0f90b4c86ed
Rayelle <equation0703@naver.com>
2024-01-14 14:34:00 +0900

    쿼터니언 코드

--- Assets/Scripts/Quaternion/SetRotation.cs
+++ Assets/Scripts/Quaternion/SetRotation.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetRotation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class SetRotation : MonoBehaviour{
+    public Transform targetTransform;
+    void Start(){
+        //Vector3를 거치지않고 Quaternion끼리 바로 각도를 증가시킬 수 있다
+        Quaternion originRotation = Quaternion.Euler(new Vector3(45,0,0));
+        Quaternion plusRotation = Quaternion.Euler(new Vector3(30,0,0));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //두 쿼터니언끼리 곱하면 값이 더해진다
+        Quaternion targetRotation = originRotation * plusRotation;
+        transform.rotation = targetRotation;//75도
     }
 }
